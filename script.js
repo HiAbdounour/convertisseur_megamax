168f2484d2a3c2e282e82b1e1bcbba1746886b7e
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-01-11 10:46:03 +0100

    Add other checking functions

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 // Functions for ensuring validity of inputs
+function isValidHexa(nb){
+    return /^[0-9A-Fa-f]*$/.test(nb);
+}
+
+function isValidDuodeca(nb){
+    // to complete
+    pass
+}
+
 function isValidDecimal(nb){
     return /^\d*$/.test(nb);
 }
 
 function isValidOctal(nb){
     return /^[0-7]*$/.test(nb);
+}
+
+function isValidBinary(nb){
+    return /^[0-1]*$/.test(nb);
+}
+
+function isValidBCD(nb){
+    // to complete
+    pass
+}
+
+function isValidTernary(nb){
+    // to complete
+    pass
+}
+
+function isValidASCII(nb){
+    // to complete
+    pass
+}
+
+function isValidArabic(nb){
+    // to complete
+    pass
 }
