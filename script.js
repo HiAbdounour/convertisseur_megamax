9f9ff417577fae8a9489baa262e1902ef567d153
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-01-11 16:11:27 +0100

    Remplace raw functions by parseInt and Number()

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function checkValidity(nb,base){
         case "ternary": return isValidTernary(nb);
         case "ascii": return isValidASCII(nb);
         case "arabic": return isValidArabic(nb);
-        default: {console.log("Unrecognized base"); return false;}
+        default: {console.log("Unrecognized system"); return false;}
     }
 }
 
@@ -83,95 +83,17 @@ function groupByX(nb,x){
   return chunks;
 }
 
+
 /*
 * Functions for converting
 *
 */
-function binaryToOctal(nb){
-    const table = {
-        "000":"0",
-        "001":"1",
-        "010":"2",
-        "011":"3",
-        "100":"4",
-        "101":"5",
-        "110":"6",
-        "111":"7"
-    }
-    const groups = groupByX(nb,3);
-    let valueOctal = "";
-    for(let i=0; i<groups.length; i+=1){
-        valueOctal = valueOctal + table[groups[i]];
-    }
-    return valueOctal;
+function awesomeConvertorFromDecimal(nb,toBase){
+    return Number(nb).toString(toBase).toUpperCase();
 }
 
-function binaryToHexa(nb){
-    const table = {
-        "0000":"0","1000":"8",
-        "0001":"1","1001":"9",
-        "0010":"2","1010":"A",
-        "0011":"3","1011":"B",
-        "0100":"4","1100":"C",
-        "0101":"5","1101":"D",
-        "0110":"6","1110":"E",
-        "0111":"7","1111":"F"
-    }
-    const groups = groupByX(nb,4);
-    let valueHexa = "";
-    for(let i=0; i<groups.length; i+=1){
-        valueHexa = valueHexa + table[groups[i]];
-    }
-    return valueHexa;
-}
-
-function binaryToDecimal(nb){
-    let valueDecimal = 0;
-    const sizex = nb.length;
-    for(let i=0; i<sizex; i+=1){
-        valueDecimal += parseInt(nb[i])*2**(sizex-i-1);
-    }
-    return String(valueDecimal);
-}
-
-function hexaToBinary(nb){
-    const table = {
-        "0":"0000","8":"1000",
-        "1":"0001","9":"1001",
-        "2":"0010","A":"1010",
-        "3":"0011","B":"1011",
-        "4":"0100","C":"1100",
-        "5":"0101","D":"1101",
-        "6":"0110","E":"1110",
-        "7":"0111","F":"1111"
-    }
-    let valueBinary = "";
-    for(let i=0; i<nb.length; i+=1){
-        valueBinary = valueBinary + table[nb[i]];
-    }
-    return valueBinary;
-}
-
-function octalToBinary(nb){
-    const table = {
-        "0":"000",
-        "1":"0001",
-        "2":"010",
-        "3":"011",
-        "4":"100",
-        "5":"101",
-        "6":"110",
-        "7":"111"
-    }
-    let valueBinary = "";
-    for(let i=0; i<nb.length; i+=1){
-        valueBinary = valueBinary + table[nb[i]];
-    }
-    return valueBinary;
-}
-
-function decimalToBinary(nb){
-    return Number(nb).toString(2);
+function awesomeConvertorToDecimal(nb,fromBase){
+    return parseInt(nb,fromBase).toString();
 }
 
 
@@ -195,31 +117,30 @@ function writeIntoX(nb,where){
 function mainx(nb,base){
     if(checkValidity(nb,base) && nb){
         const originBase = base;
-        // we convert to binary (easy to convert to other systems)
+        // we convert to decimal (easy to convert to other systems)
         const root = (()=>{
             switch(base){
-                case "hexa": return hexaToBinary(nb);
+                case "hexa": return awesomeConvertorToDecimal(nb,16);
                 case "duodecimal": return;
-                case "decimal": return decimalToBinary(nb);
-                case "octal": return octalToBinary(nb);
+                case "decimal": return nb;
+                case "octal": return awesomeConvertorToDecimal(nb,8);
                 case "cbinary"||"sbinary"||"gray": return;
-                case "ubinary": return nb;
+                case "ubinary": return awesomeConvertorToDecimal(nb,2);
                 case "bcd": return;
                 case "ternary": return;
                 case "ascii": return;
                 case "arabic": return;
-                default: return;
             }
         })();
         ids.forEach(idx=>{
             const convertedValue = (()=>{
-                if(idx=='ubinary') return root;
+                if(idx=='decimal') return root;
                 if(idx==originBase) return nb;
-                switch(base){
-                    case "hexa": return binaryToHexa(root);
+                switch(idx){
+                    case "hexa": return awesomeConvertorFromDecimal(root,16);
                     case "duodecimal": return "";
-                    case "decimal": return binaryToDecimal(root);
-                    case "octal": return binaryToOctal(root);
+                    case "octal": return awesomeConvertorFromDecimal(root,8);
+                    case "ubinary": return awesomeConvertorFromDecimal(root,2);
                     case "cbinary"||"sbinary"||"gray": return "";
                     case "bcd": return "";
                     case "ternary": return "";
